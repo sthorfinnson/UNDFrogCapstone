94a52e4bb633f20f461b80e821f89d28ab0b102b
Sean <sthorfinnson@gmail.com>
2013-11-12 00:43:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,12 +37,23 @@ int main( int argc, char** argv )
     return(0);
 }
 
+
+
 /** @function thresh_callback */
 void thresh_callback(int, void* )
 {
     Mat canny_output;
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
+    
+    
+    int rows = src.rows;
+    int cols = src.cols;
+    
+    cv::Size s = src.size();
+    rows = s.height;
+    cols = s.width;
+    int middle = cols/2;
     
     /// Detect edges using canny
     Canny( src_gray, canny_output, thresh, thresh*2, 3 );
@@ -57,7 +68,10 @@ void thresh_callback(int, void* )
     ///  Get the mass centers:
     vector<Point2f> mc( contours.size() );
     for( int i = 0; i < contours.size(); i++ )
-    { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
+    { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
+        
+    }
+    
     
     /// Draw contours
     Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
@@ -66,6 +80,37 @@ void thresh_callback(int, void* )
         Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
         drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
         circle( drawing, mc[i], 4, color, -1, 8, 0 );
+        double res= cv::norm(mc[i]-mc[i+1]);                    //Distance between two points is less than 300 pixels
+        if(res < 300){
+            double res2 = cv::norm(mc[i].y - mc[i+1].y);        //Vertical distance between points is less than 10 pixels
+            if(res2 < 10){
+                
+                
+            
+                line( drawing, mc[i], mc[i+1],color, 1, 8, 0);  //Draw line horizontally between the biggest contours
+                Point pt3(((mc[i+1].x - mc[i].x)/2)+mc[i].x, 0);
+                Point pt4(((mc[i+1].x - mc[i].x)/2)+mc[i].x,s.height);
+                int res3 = cv::norm(pt4.x - middle);
+                res3 = abs(res3);
+                printf("Res 3 x\t%.2d\n", res3);
+                printf("pt4.x %d\n", pt4.x);
+                printf("pt3.x %d\n", pt3.x);
+                printf("middle %d\n", middle);
+                if(res3 < 50){                                  //If the vertical line is less than 50 pixels away from midpoint of image--Draw
+                    line( drawing, pt3, pt4, color,1,8,0);
+                    printf("First x\t%.2f\n", mc[i].x);
+                    printf("First y\t%.2f\n", mc[i].y);
+                    printf("Second x\t%.2f\n", mc[i+1].x);
+                    printf("Second y\t%.2f\n", mc[i+1].y);
+                }
+                
+                
+            
+            }
+            
+            
+        };
+        
     }
     
     /// Show in a window
@@ -76,9 +121,9 @@ void thresh_callback(int, void* )
     printf("\t Info: Area and Contour Length \n");
     for( int i = 0; i< contours.size(); i++ )
     {
-        printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
+       // printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
         Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
         drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
         circle( drawing, mc[i], 4, color, -1, 8, 0 );
     }
-} 
+}
